1d845a3a45caa6a062584af75188773190f472a1
Dibas Kumar Kar <dkar@internal.tavisca.com>
2021-05-31 23:53:03 +0530

    boston dataset project

--- MLops_test.py
+++ MLops_test.py
@@ -11,33 +11,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
 boston = load_boston()
+#print(boston.DESCR)
 features = pd.DataFrame(boston.data,columns=boston.feature_names)
+#features['CRIM']
 target = pd.DataFrame(boston.target,columns=['MEDV'])
+#print(max(target['target']))
+#print(min(target['target']))
 df = pd.concat([features,target],axis=1)
-
+df.describe() # to describe
 # calculate correlation between every column on the data
 corr = df.corr(method='pearson')
+#corr
 
 #take absolute values of correlation
+
 corrs = [abs(corr[attr]['MEDV']) for attr in list(features)]
 
-#make a list of pairs [(features),corrs]
-l = list(zip(list(features),corrs))
+#make a list of pairs [(corr,features)]
 
-with open("correlation.txt", 'w') as outfile:
-        outfile.write("list of pairs [(features),corrs]: %s\n" % l)
-
+l = list(zip(corrs,list(features)))
 #sort the list of pairs in reverse order woth correlation values as the key of sorting
 l.sort(key=lambda x:x[0], reverse= True)
+#l
+#unzip the pairs to two lists
+#zip(*l) takes a list that looks like [[a,b,c],[d,e,f],[g,h,i]] and returns [[a,d,g],[b,e,h],[c,f,i]]
 
-#unzip the pairs to two lists 
-# zip(*l) takes a list that looks like [[a,b,c],[d,e,f],[g,h,i]]and returns [[a,d,g],[b,e,h],[c,f,i]]
 corrs,labels = list(zip(*l))
 
+#corrs
+
+labels
+
 #plot correlation with respect to the target variable as a bar graph
+
 index = np.arange(len(labels))
 plt.figure(figsize =(15,5))
+
 plt.bar(index,corrs,width=0.5)
+
 plt.xlabel('features')
 plt.ylabel("correlation with target values")
 plt.xticks(index,labels)
@@ -45,7 +56,6 @@ plt.savefig("feature",dpi=120)
 plt.show()
 plt.close()
 
-# plot the distribution of the target variable "MEDV"
 plt.xlabel('target')
 sns.set(rc={'figure.figsize':(11.7,8.27)})
 save = sns.distplot(target, bins=30)
@@ -53,12 +63,18 @@ plt.savefig("target",dpi=120)
 plt.show()
 plt.close()
 
-# Train and Test the data Model
-X = features
+X = pd.DataFrame(boston.data,columns=boston.feature_names)
+#X = pd.DataFrame(np.c_[boston['CRIM'], boston['RM']], columns = ['CRIM','RM'])
 Y = target
+
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=5)
 
-#Fit a model on the train section 
+# print(X_train.shape)
+# print(X_test.shape)
+# print(Y_train.shape)
+# print(Y_test.shape)
+
+#Fit a model on the train section
 #Set random seed
 seed = 42
 regr = RandomForestRegressor(max_depth=2, random_state=seed)
@@ -69,18 +85,24 @@ train_score = regr.score(X_train, Y_train) * 100
 # Report test set score
 test_score = regr.score(X_test, Y_test) * 100
 
+# print("Training variance explained: {%s}\n" % train_score)
+
+# print("Test variance explained: {%s}\n" % test_score)
+
+
 with open("metrics.txt", 'w') as outfile:
         outfile.write("Training variance explained: {%s}\n" % train_score)
         outfile.write("Test variance explained: {%s}\n" % test_score)
 
-# Linear Regrassion
 lin_model = LinearRegression()
 lin_model.fit(X_train, Y_train)
 
 # model evaluation for training set
 y_train_predict = lin_model.predict(X_train)
+#print(y_train_predict[:5])
 rmse_training = (np.sqrt(mean_squared_error(Y_train, y_train_predict)))
 r2_training = r2_score(Y_train, y_train_predict)
+
 
 # model evaluation for testing set
 y_test_predict = lin_model.predict(X_test)
