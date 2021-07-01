ca48c3c8d32228a1c84d37e4d86fd7d18f0f67c3
Dibas Kumar Kar <dkar@internal.tavisca.com>
2021-07-01 21:23:56 +0530

    demo

--- Unix.py
+++ Unix.py
@@ -1,5 +1,4 @@
 disk = os.subprocess("df -h")
-with open("evalution.txt", 'w') as outfile:
+with open("demo.txt", 'w') as outfile:
         outfile.write(" Demo testing: {%s}\n\n" % disk)
 
-        
